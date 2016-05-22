51fb0901d324d04c066f627bb4ad41985aec1ebf
ywl <ywlbupt@163.com>
2016-05-22 21:10:20 +0800

    commit @ windows

--- prj_python/Hello_python.py
+++ prj_python/Hello_python.py
@@ -1,7 +1,20 @@
 # coding:utf-8
 
-import requests
-r = requests.get('https://www.baidu.com')
-print(r.text)
-print(r.encoding)
-print(r.status_code)
+squares = []
+for x in range (10):
+    squares.append(x**2)
+
+print(squares)
+
+squares = [x**2 for x in range(10)]
+
+print(squares)
+
+numbers = [x for x in range(100) if x % 3 == 0]
+
+print(numbers)
+
+noprimes = [j for i in range(2, 8) for j in range(i*2, 50, i)]
+primes = [x for x in range(2, 50) if x not in noprimes]
+
+print(primes)
