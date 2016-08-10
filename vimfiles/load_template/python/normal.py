5e2862f31c44ce64e5949a9d3e36f40905fa00b2
ywl-E420 <ywlbupt@163.com>
2016-08-10 08:55:19 +0800

    bazaoluanqi

---
+++ vimfiles/load_template/python/normal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#*=============================================================================
+# Author :
+# Version :
+# LastChange:      
+# Description:     
+``` 
+
+```
+#*=============================================================================
+
+TEMPLATE_CURSOR
+
+if __name__=='__main__':
+    print("Hello World")
+    pass
+
+EXE_BEGIN_TEMPLATE
+
+
+
+EXE_END_TEMPLATE
