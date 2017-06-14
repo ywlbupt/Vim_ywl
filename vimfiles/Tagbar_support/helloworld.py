5b7ed35b9cc2d624c46f25a0b74438baec8f767f
ywl@XM <ywlbupt@163.com>
2017-06-14 19:12:57 +0800

    0614@XM

---
+++ vimfiles/Tagbar_support/helloworld.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+print('hello world!')
+time.sleep(10)  #让print结果在窗口显示10s，便于执行exe文件时看到效果
---
+++ vimfiles/Tagbar_support/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=["markdown2ctags.py"])
+# setup(console=["helloworld.py"])
